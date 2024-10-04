a5c937b895bef16fa70461bf055aae487df5a9b7
Alex To <alexto@student.unsw.edu.au>
2024-10-05 03:40:00 +1000

    changed underlying data representation of file system and created helper functions for new structure

--- folder/folder.go
+++ folder/folder.go
@@ -1,6 +1,8 @@
 package folder
 
 import (
+	"strings"
+
 	"github.com/gofrs/uuid"
 )
 
@@ -52,9 +54,17 @@ type driver struct {
 	// folders []Folder
 }
 
-func NewDriver(folders []Folder) IDriver {
-	orgs := map[uuid.UUID]Organization{}
+func findFileNode(folders []FileNode, name string) *FileNode {
+	for _, f := range folders {
+		if f.file.Name == name {
+			return &f
+		}
+	}
 
+	return nil
+}
+
+func generateFileNodes(folders []Folder, orgs map[uuid.UUID]Organization) {
 	for _, f := range folders {
 		_, exists := orgs[f.OrgId]
 		if !exists {
@@ -64,6 +74,46 @@ func NewDriver(folders []Folder) IDriver {
 		org.folders = append(org.folders, NewFileNode(f))
 		orgs[f.OrgId] = org
 	}
+}
+
+func generateNodeParents(folders []FileNode) {
+	for i, fileNode := range folders {
+		curr_path := fileNode.file.Paths
+		path_sections := strings.Split(curr_path, ".")
+		if len(path_sections) > 1 {
+			parent := findFileNode(folders, path_sections[len(path_sections)-2])
+			folders[i].parent = parent
+			parent.children = append(parent.children, &fileNode)
+		}
+	}
+}
+
+func generateNodeChildren(folders []FileNode, parentNode FileNode) []*FileNode {
+	children := []*FileNode{}
+	for _, childNode := range folders {
+		if childNode.parent != nil && childNode.parent.file.Name == parentNode.file.Name {
+			children = append(children, &childNode)
+		}
+	}
+	return children
+}
+
+func generateOrgs(folders []Folder) map[uuid.UUID]Organization {
+	orgs := map[uuid.UUID]Organization{}
+	generateFileNodes(folders, orgs)
+
+	for _, org := range orgs {
+		generateNodeParents(org.folders)
+		for i, fileNode := range org.folders {
+			org.folders[i].children = generateNodeChildren(org.folders, fileNode)
+		}
+	}
+
+	return orgs
+}
+
+func NewDriver(folders []Folder) IDriver {
+	orgs := generateOrgs(folders)
 
 	return &driver{
 		orgs: orgs,
