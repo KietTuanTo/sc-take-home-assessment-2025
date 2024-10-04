66ada292c3545c779b440aed12f26c7f392ab435
Alex To <alexto@student.unsw.edu.au>
2024-10-04 17:55:49 +1000

    created test for GetFoldersByOrgID and testing template for GetAllChildFolders

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -1,6 +1,7 @@
 package folder_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/georgechieng-sc/interns-2022/folder"
@@ -17,13 +18,71 @@ func Test_folder_GetFoldersByOrgID(t *testing.T) {
 		folders []folder.Folder
 		want    []folder.Folder
 	}{
-		// TODO: your tests here
+		{
+			name:  "Test with Valid UUID",
+			orgID: uuid.FromStringOrNil(folder.DefaultOrgID),
+			folders: []folder.Folder{
+				{
+					Name:  "alpha",
+					OrgId: uuid.FromStringOrNil(folder.DefaultOrgID),
+					Paths: "alpha",
+				},
+				{
+					Name:  "beta",
+					OrgId: uuid.FromStringOrNil(folder.DefaultOrgID),
+					Paths: "beta",
+				},
+				{
+					Name:  "gamma",
+					OrgId: uuid.Must(uuid.FromString("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7")),
+					Paths: "gamma",
+				},
+			},
+
+			want: []folder.Folder{
+				{
+					Name:  "alpha",
+					OrgId: uuid.FromStringOrNil(folder.DefaultOrgID),
+					Paths: "alpha",
+				},
+				{
+					Name:  "beta",
+					OrgId: uuid.FromStringOrNil(folder.DefaultOrgID),
+					Paths: "beta",
+				},
+			},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// f := folder.NewDriver(tt.folders)
-			// get := f.GetFoldersByOrgID(tt.orgID)
+			f := folder.NewDriver(tt.folders)
+			get := f.GetFoldersByOrgID(tt.orgID)
 
+			if !reflect.DeepEqual(get, tt.want) {
+				t.Errorf("GetFoldersByOrgID() = %v, want %v", get, tt.want)
+			}
+		})
+	}
+}
+
+func Test_folder_GetAllChildFolders(t *testing.T) {
+	t.Parallel()
+	tests := [...]struct {
+		name    string
+		orgID   uuid.UUID
+		folders []folder.Folder
+		want    []folder.Folder
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			get := f.GetFoldersByOrgID(tt.orgID)
+
+			if !reflect.DeepEqual(get, tt.want) {
+				t.Errorf("GetFoldersByOrgID() = %v, want %v", get, tt.want)
+			}
 		})
 	}
 }
