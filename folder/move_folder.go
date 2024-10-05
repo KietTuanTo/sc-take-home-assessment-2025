e6701fc44efd97df455bb2c4628a714863330b89
Alex To <alexto@student.unsw.edu.au>
2024-10-05 17:53:21 +1000

    created documentation for move_folder.go, such as function descriptions

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -6,7 +6,10 @@ import (
 	"github.com/gofrs/uuid"
 )
 
-func findFolder(name string, orgs map[uuid.UUID]Organization) (*FileNode, string) {
+// FindFolder returns a pointer to the FileNode and a string
+// representation of its UUID, given the name of the Folder it
+// contains
+func FindFolder(name string, orgs map[uuid.UUID]Organization) (*FileNode, string) {
 	for orgID, org := range orgs {
 		for _, fileNode := range org.folders {
 			if fileNode.file.Name == name {
@@ -18,11 +21,13 @@ func findFolder(name string, orgs map[uuid.UUID]Organization) (*FileNode, string
 	return nil, ""
 }
 
-func checkIsChild(src *FileNode, dst *FileNode) bool {
+// CheckIsChild returns a boolean stating whether the
+// given 'dst' FileNode is a child of 'src'
+func CheckIsChild(src *FileNode, dst *FileNode) bool {
 	for _, childNode := range src.children {
 		if childNode.file.Name == dst.file.Name {
 			return true
-		} else if checkIsChild(childNode, dst) {
+		} else if CheckIsChild(childNode, dst) {
 			return true
 		}
 	}
@@ -30,7 +35,10 @@ func checkIsChild(src *FileNode, dst *FileNode) bool {
 	return false
 }
 
-func removeChild(parentNode *FileNode, fileToRemove string) []*FileNode {
+// RemoveChild returns the children of 'parentNode'
+// after removing the FileNode containing the
+// Folder with name 'fileToRemove'
+func RemoveChild(parentNode *FileNode, fileToRemove string) []*FileNode {
 	for i, fileNode := range parentNode.children {
 		if fileNode.file.Name == fileToRemove {
 			return append(parentNode.children[:i], parentNode.children[:i+1]...)
@@ -40,14 +48,18 @@ func removeChild(parentNode *FileNode, fileToRemove string) []*FileNode {
 	return parentNode.children
 }
 
-func changeChildPaths(parentNode *FileNode) {
+// ChangeChildPaths changes all paths of the Folders
+// contained within child FileNodes of 'parentNode'
+func ChangeChildPaths(parentNode *FileNode) {
 	for _, childNode := range parentNode.children {
 		childNode.file.Paths = parentNode.file.Paths + "." + childNode.file.Name
-		changeChildPaths(childNode)
+		ChangeChildPaths(childNode)
 	}
 }
 
-func createFolderSlice(orgs map[uuid.UUID]Organization) []Folder {
+// CreateFolderSlice returns a slice containing all the folders
+// stored within the drive
+func CreateFolderSlice(orgs map[uuid.UUID]Organization) []Folder {
 	folders := []Folder{}
 	for _, org := range orgs {
 		for _, fileNode := range org.folders {
@@ -58,13 +70,15 @@ func createFolderSlice(orgs map[uuid.UUID]Organization) []Folder {
 	return folders
 }
 
+// MoveFolder moves a folder with 'name' and all its children
+// to a different parent folder
 func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 	if name == dst {
 		return []Folder{}, errors.New("error: cannot move a folder to itself")
 	}
 
-	srcFolder, srcID := findFolder(name, f.orgs)
-	dstFolder, dstID := findFolder(dst, f.orgs)
+	srcFolder, srcID := FindFolder(name, f.orgs)
+	dstFolder, dstID := FindFolder(dst, f.orgs)
 	if srcFolder == nil {
 		return []Folder{}, errors.New("error: source folder does not exist")
 	}
@@ -74,21 +88,21 @@ func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
 	if srcID != dstID {
 		return []Folder{}, errors.New("error: cannot move a folder to a different organization")
 	}
-	if checkIsChild(srcFolder, dstFolder) {
+	if CheckIsChild(srcFolder, dstFolder) {
 		return []Folder{}, errors.New("error: cannot move a folder to a child of itself")
 	}
 
 	srcParent := srcFolder.parent
 	if srcParent != nil {
-		srcParent.children = removeChild(srcParent, srcFolder.file.Name)
+		srcParent.children = RemoveChild(srcParent, srcFolder.file.Name)
 	}
 
 	srcFolder.parent = dstFolder
 	dstFolder.children = append(dstFolder.children, srcFolder)
 	srcFolder.file.Paths = srcFolder.parent.file.Paths + "." + srcFolder.file.Name
 
-	changeChildPaths(srcFolder)
+	ChangeChildPaths(srcFolder)
 
-	folderList := createFolderSlice(f.orgs)
+	folderList := CreateFolderSlice(f.orgs)
 	return folderList, nil
 }
