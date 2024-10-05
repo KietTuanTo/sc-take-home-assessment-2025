21899d27747c7e9ccf6919c546d3518669906933
Alex To <alexto@student.unsw.edu.au>
2024-10-05 14:20:15 +1000

    changed test for GetAllChildFolders

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -114,7 +114,7 @@ func Test_folder_GetAllChildFolders(t *testing.T) {
 				{
 					Name:  "gamma",
 					OrgId: uuid.FromStringOrNil(folder.DefaultOrgID),
-					Paths: "alpha.gamma",
+					Paths: "alpha.beta.gamma",
 				},
 			},
 
@@ -127,7 +127,7 @@ func Test_folder_GetAllChildFolders(t *testing.T) {
 				{
 					Name:  "gamma",
 					OrgId: uuid.FromStringOrNil(folder.DefaultOrgID),
-					Paths: "alpha.gamma",
+					Paths: "alpha.beta.gamma",
 				},
 			},
 		},
