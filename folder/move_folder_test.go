d130332df066e52dc98712ca54377f6dfc28413b
Alex To <alexto@student.unsw.edu.au>
2024-10-05 16:20:41 +1000

    created testing template for MoveFolder

--- folder/move_folder_test.go
+++ folder/move_folder_test.go
@@ -1,9 +1,38 @@
 package folder_test
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/georgechieng-sc/interns-2022/folder"
+	"github.com/gofrs/uuid"
 )
 
 func Test_folder_MoveFolder(t *testing.T) {
-	// TODO: your tests here
+	t.Parallel()
+	tests := [...]struct {
+		name    string
+		src     string
+		dst     string
+		orgID   uuid.UUID
+		folders []folder.Folder
+		want    []folder.Folder
+		err     error
+	}{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			get, err := f.MoveFolder(tt.src, tt.dst)
+
+			if !reflect.DeepEqual(get, tt.want) {
+				t.Errorf("MoveFolder() = %v, want %v for output", get, tt.want)
+			}
+
+			if tt.err != err {
+				t.Errorf("MoveFolder() = %v, want %v for error", err, tt.err)
+			}
+		})
+	}
+
 }
